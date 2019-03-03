567bd1a94e127088ca6f28e1ccaa875cb15e1dda
busterfall <busterfall95@gmail.com>
2019-03-03 22:46:54 +0200

    rm some files

--- Lesson_1_Task_1.py
+++
@@ -1,9 +0,0 @@
-'''Task 1'''
-
-var_1 = input('What is your name?\n')
-var_2 = input('How old are you?\n')
-var_3 = input('Where do you live?\n')
-
-print('Hello, ' + var_1 + '!')
-print('Your age is ' + var_2 + '.')
-print('You live in ' + var_3 + '.')
--- Lesson_1_Task_2.py
+++
@@ -1,6 +0,0 @@
-'''Task 2'''
-
-a = int(input("What 'a' means?\n"))
-b = int(input("What 'b' means?\n"))
-print(a + b)
-print(a - b)
